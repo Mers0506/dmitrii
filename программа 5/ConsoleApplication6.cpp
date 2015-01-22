c5a37087fb9602abde9126d336a2c6d3ee6313b9
Mers0506 <Ss1996q@mail.ru>
2015-01-23 01:52:25 +0300

    последняя версия

--- программа 5/ConsoleApplication6.cpp
+++ программа 5/ConsoleApplication6.cpp
@@ -1,82 +1,56 @@
-
-
-
+// ConsoleApplication6.cpp: определяет точку входа для консольного приложения.
+//
 
 #include "stdafx.h"
 
 #include <iostream>
 #include <fstream>
-#include <string>
 using namespace std;
 int main(void)
 {
-	setlocale(LC_ALL, "Russian");
-	char let;
-	string slovo;
-	string pre ;
-	string pro, mid;
-	int n, d, a, m;
+	setlocale(LC_ALL, "rus");
+	int n, m, max;
 	ifstream in("in.txt");
 	if (!in.is_open())
 	{
-		cout << " in.txt" << endl;
+		cout << "Not open in.txt" << endl;
 		return -4;
 	}
-	in.get(let);
+	in >> n >> m;
+	int** A = new int*[m + 2];
+	for (int i = 0; i < n + 2; i++)
+	{
+		A[i] = new int[m + 2];
+	}
+	for (int i = 0; i < m + 2; i++)
+	{
+		for (int j = 0; j < n + 2; j++)
+		{
+			if (i != 0 && i != m + 1 && j != 0 && j != n + 1)
+			{
+				in >> A[i][j];
+			}
+			else
+			{
+				A[i][j] = 0;
+			}
+		}
+	}
+
 	ofstream out("out.txt");
 	if (!out.is_open())
 	{
-		cout << "out.txt" << endl;
+		cout << "Not create out.txt" << endl;
 		return -4;
 	}
-	a = 0;
-	n = 0;
-	d = 0;
-	m = 0;
-	
-	while (!in.eof())
+	for (int i = 1; i < m + 1; i++)
 	{
-		in.get(let);
-		a = a + 1;
-		n = n + 1;
-		pro = pro + let;
-		mid = mid + let;
-		if (let == ' ' || let == '?' || let == '!' || let == '.' || let == ',' )
+		for (int j = 1; j < n + 1; j++)
 		{
-			if (d<n)
-			{
-				
-				slovo = pro;
-				d = n;
-				pro.clear();
-				n = 0;
-			}
-			else if (d == n)
-			{
-				
-				slovo = slovo + pro;
-					n = 0;
-					pro.clear();
-			}
-			else {
-				n = 0;
-				pro.clear();
-			}
-			
-			
-			if (let == '?' || let == '!' || let == '.' && m < a)
-			{
-				pre = mid;
-				m = a;
-				mid.clear();
-				a = 0;
-			}
+			out << A[i - 1][j - 1] + A[i - 1][j] + A[i - 1][j + 1] + A[i][j - 1] + A[i][j] + A[i][j + 1] + A[i + 1][j - 1] + A[i + 1][j] + A[i + 1][j + 1] << ' ';
 		}
-		
+		out << endl;
 	}
-
-	out << "самое длинное слово." << endl << slovo << endl;
-	out << "самое длинное предложение." << endl << pre << endl;
-
+	system("pause");
 	return 0;
 }
